22bb0866d716b1361145b990bb073827430dc716
rgcas <rgcascante@outlook.com>
2025-09-12 16:10:31 -0500

    Feature: Server

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require('express'), 
+    mongoose = require('mongoose'), 
+    cors = require("cors");
+
+const app = express()
+app.use(cors());
+mongoose.connect(process.env.MONGO_URI)
+
+app.use(express.json());
+
+const routes = require('./src/routes/index-routes')
+
+app.use('', routes);
+
+app.listen(3000, () => {
+    console.log("Server listen")
+})
